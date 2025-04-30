f8c38f104f68476b2518c1ea5cf0b2be6d873f2c
amanpatodia1 <aman.patodia@dhani.com>
2025-04-30 16:07:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tweetsapp/data/repository/TweetsRepository.kt
@@ -0,0 +1,17 @@
+package com.example.tweetsapp.data.repository
+
+import com.example.tweetsapp.models.TweetsListItem
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
+
+interface TweetsRepository {
+
+    @GET("/v3/b/6809a1ea8561e97a500645da?meta=false")
+    suspend fun getTweets(@Header("X-JSON-Path") category : String) : Response<List<TweetsListItem>>
+
+    @GET("/v3/b/6809a1ea8561e97a500645da?meta=false")
+    @Headers("X-JSON-Path : tweets..category")
+    suspend fun getCategories() : Response<List<String>>
+}
---
+++ app/src/main/java/com/example/tweetsapp/models/TweetsListItem.kt
@@ -0,0 +1,6 @@
+package com.example.tweetsapp.models
+
+data class TweetsListItem(
+    val category: String,
+    val tweet: String
+)
